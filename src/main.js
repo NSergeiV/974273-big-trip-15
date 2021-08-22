56f6d4f35588eff6473838cc88c5ab3be30b7321
NSergeiV <ttniseva@yandex.ru>
2021-08-23 01:48:53 +0300

    Приступил к новому заданию - 3.12. Революция или эволюция?

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import flatpickr from 'flatpickr';
-import {headerRoutePrice} from './view/header-route-price.js';
-import {headerMenu} from './view/header-menu.js';
+
+import {createHeaderRoutePriceTemplate} from './view/header-route-price.js';
+import HeaderMenu from './view/header-menu.js';
 import {headerFilter} from './view/header-filter.js';
 import {mainTripSort} from './view/main-sort.js';
 import {listPoints} from './view/list.js';
 import {generateTask} from './mock/task.js';
-import {compare} from './utils.js';
+import {compare, renderElement, RenderPosition} from './utils.js';
 import {formEditingPoint} from './view/form-editing-point.js';
 import {eventSectionOffers} from './view/event-section-offers.js';
 import {eventSectionDestination} from './view/event-section-destination.js';
@@ -18,10 +19,6 @@ const tasks = new Array(TASK_COUNT).fill().map(generateTask);
 
 const tasksSort = tasks.slice().sort((a, b) => compare(a.dateStart, b.dateStart));
 
-const paste = (container, template, place) => {
-  container.insertAdjacentHTML(place, template);
-};
-
 const siteHeader = document.querySelector('header');
 const siteBlockMain = document.querySelector('main');
 const siteHeaderElementTripMain = siteHeader.querySelector('.trip-main');
@@ -29,30 +26,30 @@ const siteHeaderElementNavigation = siteHeader.querySelector('.trip-controls__na
 const siteHeaderElementFilter = siteHeader.querySelector('.trip-controls__filters');
 const siteMainSection = siteBlockMain.querySelector('.trip-events');
 
-paste(siteHeaderElementTripMain, headerRoutePrice(), 'afterbegin');
-paste(siteHeaderElementNavigation, headerMenu(), 'beforeend');
-paste(siteHeaderElementFilter, headerFilter(), 'beforeend');
-paste(siteMainSection, mainTripSort(), 'afterbegin');
-paste(siteMainSection, listPoints(), 'beforeend');
+renderElement(siteHeaderElementTripMain, createHeaderRoutePriceTemplate(), RenderPosition.AFTERBEGIN);
+renderElement(siteHeaderElementNavigation, new HeaderMenu().getElement(), RenderPosition.BEFOREEND);
+renderElement(siteHeaderElementFilter, headerFilter(), RenderPosition.BEFOREEND);
+renderElement(siteMainSection, mainTripSort(), RenderPosition.AFTERBEGIN);
+renderElement(siteMainSection, listPoints(), RenderPosition.BEFOREEND);
 
 const tripEventsList = siteMainSection.querySelector('.trip-events__list');
 
-paste(tripEventsList, formEditingPoint(tasksSort[0]), 'afterbegin');
+renderElement(tripEventsList, formEditingPoint(tasksSort[0]), RenderPosition.AFTERBEGIN);
 const eventDetails = document.querySelector('.event__details');
 
 if (tasksSort[0].eventOffer.length !== 0) {
-  paste(eventDetails, eventSectionOffers(), 'afterbegin');
+  renderElement(eventDetails, eventSectionOffers(), RenderPosition.AFTERBEGIN);
   const eventAvailableOffers = eventDetails.querySelector('.event__available-offers');
   const offers = tasksSort[0].eventOffer;
   offers.forEach((offer) => {
     const key = Object.keys(offer);
     const property = Object.values(offer);
-    paste(eventAvailableOffers, eventOfferSelector(key[0], property[0]), 'afterbegin');
+    renderElement(eventAvailableOffers, eventOfferSelector(key[0], property[0]), RenderPosition.AFTERBEGIN);
   });
 }
 
 if (tasksSort[0].description.length !== 0 || tasksSort[0].eventPhoto !== null) {
-  paste(eventDetails, eventSectionDestination(tasksSort[0].description), 'beforeend');
+  renderElement(eventDetails, eventSectionDestination(tasksSort[0].description), RenderPosition.BEFOREEND);
   if (tasksSort[0].eventPhoto !== null) {
     const eventPhotosTape = eventDetails.querySelector('.event__photos-tape');
     const photos = tasksSort[0].eventPhoto;
@@ -67,7 +64,7 @@ if (tasksSort[0].description.length !== 0 || tasksSort[0].eventPhoto !== null) {
 }
 
 for (let i = 1; i < tasksSort.length; i++) {
-  paste(tripEventsList, routePoint(tasksSort[i]), 'beforeend');
+  renderElement(tripEventsList, routePoint(tasksSort[i]), RenderPosition.BEFOREEND);
 }
 
 const configFlatpickr = {
